64de999c75642dbf95afd7ffc9a22a7543026917
Darius Vasionis <darius@vasionis.eu>
2022-11-08 10:32:11 +0200

    Tvarkeme admin.py faila

--- ptu5_library/library/admin.py
+++ ptu5_library/library/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from . import models
+
+class BookInstanceInline(admin.TabularInline):
+    model = models.BookInstance
+    extra: 0
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = (BookInstanceInline, )
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('unique_id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+    readonly_fields = ('unique_id', )
+    can_delete = False
+
+    fieldsets = (
+        ('General', {'fields': ('unique_id', 'book')}),
+        ('Availability', {'fields': (('status', 'due_back'),)}),
+    )
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    # padarome galimybę rūšiuoti pagal pavardžių ir vardų laukus iš admino
+    list_display=('first_name', 'last_name')
+    list_display_links=('last_name', )
+
 
 # Register your models here.
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Genre)
+admin.site.register(models.Book, BookAdmin)
+admin.site.register(models.BookInstance, BookInstanceAdmin)
--- ptu5_library/library/models.py
+++ ptu5_library/library/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+import uuid
 
+# uuid - random generuojamas ID, kad susigeneruoti unukalų indentifikatorių
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField('name', max_length=200, help_text = "Enter name of the book genre")
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField('first_name', max_length = 50)
+    last_name = models.CharField('last_name', max_length = 50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name}, {self.last_name}"
+
+    class Meta:
+        # nurodome, kaip bus rūšiuojamas sąrašas. Dažniausiai naudojamas tuple, nes veikia 
+        # greičiau, nei sąrašai. bet naudojammi ir sąrašai
+        ordering = ['last_name', 'first_name']
+
+class Book(models.Model):
+    title = models.CharField('title', max_length=255)
+    summary = models.TextField('summary') # Text field yra neribojamas ir jo galima nerašyti
+    isbn = models.CharField('ISBN', max_length=13, null=True, blank=True,
+        help_text='<a href="https://www.isbn-international.org/content/what-isbn" target="_blank">ISBN code</a> consisting of 13 symbols')
+    author = models.ForeignKey(Author,on_delete=models.SET_NULL, null=True, blank=True)
+    genre = models.ManyToManyField(Genre, help_text = "Choose genre(s) for this book", verbose_name='genre(s)')
+
+    def __str__(self) -> str:
+        return f'{self.author} - {self.title}'
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+
+class BookInstance(models.Model):
+    unique_id = models.UUIDField('unique ID', default = uuid.uuid4, editable=False)
+    book = models.ForeignKey(Book, verbose_name='book', on_delete = models.CASCADE)
+    due_back = models.DateField('due back', null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'manage'),
+        ('t', 'taken'),
+        ('a', 'available'),
+        ('r', 'reserved'),
+    )
+
+    status = models.CharField('status', max_length=1, choices=LOAN_STATUS, default='m')
+    # price = models.DecimalField("price", max_digits=18, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f'{self.unique_id}: {self.book.title}'
+    
+    class Meta():
+        ordering = ['due_back']
